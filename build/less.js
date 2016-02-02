ce8f72852b8c96f945281ecf14370f0434223e9d
Joseph Aspelund <Joseph.Aspelund@bestbuy.com>
2016-02-01 19:08:57 -0600

    Extract base styles. Add SVG example.

--- build/less.js
+++ build/less.js
@@ -2,25 +2,49 @@ var less = require('less');
 var fs = require('fs');
 var path = require('path');
 
-var filePath = path.join(__dirname, '../less/Web-Styles.less');
+var getFileName = function(fullPath) {
+    var fileName = fullPath.replace( path.dirname(fullPath), '');
+    fileName = fileName.replace( path.extname(fileName), '' );
+    fileName = fileName.replace(/[\\\/]/, '');
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err){
-		
-		less.render(data,
-			{
-			  paths: ['./less'],  // Specify search paths for @import directives
-			  filename: 'dist/less-build.log', // Specify a filename, for better error messages
-			  compress: true          // Minify CSS output
-			},
-			function (e, output) {
-				var resultFile = path.join(__dirname, '../dist/styles.css');
-				fs.writeFile(resultFile, output.css, {encoding: 'utf-8'});
-			}
-		);
+    return fileName;
+};
 
-    }else{
-        console.log(err);
+var distPath = path.join(__dirname, '../dist');
+var buildCSS = function (lessFile) {
+    fs.readFile(lessFile, {encoding: 'utf-8'}, function(err, data){
+        if (err) {
+            throw err;
+        }
+
+        var cssFile = path.join(distPath, getFileName(lessFile));
+        cssFile +=  '.css';
+
+        less.render(data,
+            {
+              paths: ['./less'],  // Specify search paths for @import directives
+              filename: 'dist/less-build.log', // Specify a filename, for better error messages
+              compress: true          // Minify CSS output
+            },
+            function (e, output) {
+                fs.writeFile(cssFile, output.css, {encoding: 'utf-8'});
+            }
+        );
+
+    });
+};
+
+var lessHomePath = path.join(__dirname, '../less');
+fs.readdir(lessHomePath, function (err, files) {
+    if (err) {
+        throw err;
     }
 
+    files.map(function (file) {
+        return path.join(lessHomePath, file);
+    }).filter(function (file) {
+        return fs.statSync(file).isFile() && /[^[\\\/]]*(.*?)\.less$/.test(file);
+    }).forEach(function (file) {
+        buildCSS(file)
+    });
 });
